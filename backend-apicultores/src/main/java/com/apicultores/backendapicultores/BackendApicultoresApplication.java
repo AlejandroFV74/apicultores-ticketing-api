1943aeef1dc242f16c0269acbb988d2f42f21301
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-16 22:25:34 -0600

    feat: n-layers architecture set up

---
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/BackendApicultoresApplication.java
@@ -0,0 +1,13 @@
+package com.apicultores.backendapicultores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApicultoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApicultoresApplication.class, args);
+    }
+
+}
---
+++ backend-apicultores/src/test/java/com/apicultores/backendapicultores/BackendApicultoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicultores.backendapicultores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApicultoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
