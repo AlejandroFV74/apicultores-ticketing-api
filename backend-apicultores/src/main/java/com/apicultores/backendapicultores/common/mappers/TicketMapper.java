c8b9d7b4a698bbefb0b889842793e5f1458b8959
DiegoIraheta00041923 <128827201+DiegoIraheta00041923@users.noreply.github.com>
2026-06-08 15:43:05 -0600

    feat: Service for get tickets is done

--- backend-apicultores/src/main/java/com/apicultores/backendapicultores/common/mappers/TicketMapper.java
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/common/mappers/TicketMapper.java
@@ -2,6 +2,7 @@ package com.apicultores.backendapicultores.common.mappers;
 
 import com.apicultores.backendapicultores.common.TicketStatus;
 import com.apicultores.backendapicultores.domain.dto.request.CreateTicketRequest;
+import com.apicultores.backendapicultores.domain.dto.response.ticket.TicketResponse;
 import com.apicultores.backendapicultores.domain.entities.Reservation;
 import com.apicultores.backendapicultores.domain.entities.Seat;
 import com.apicultores.backendapicultores.domain.entities.Ticket;
@@ -18,5 +19,20 @@ public class TicketMapper {
                 .status(TicketStatus.ACTIVE)
                 .build();
 
+    }
+
+    public TicketResponse toDto(Ticket ticket){
+        return TicketResponse.builder()
+                .qrCode(ticket.getQrCode())
+                .status(ticket.getStatus())
+                .usedAt(ticket.getUsedAt())
+                .createdAt(ticket.getCreatedAt())
+                .seatNumber(ticket.getSeat().getSeatNumber())
+                .seatType(ticket.getSeat().getSeatType())
+                //No implementado aún, falta definir las entidades
+                .eventName(ticket.getSeat().getEvent().getName())
+                .eventDate(ticket.getSeat().getEvent().getStartDate())
+                .ownerName(ticket.getOwner().getName())
+                .build();
     }
 }
--- backend-apicultores/src/main/java/com/apicultores/backendapicultores/services/ticket/getTicketService.java
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/services/ticket/getTicketService.java
@@ -1,18 +1,38 @@
 package com.apicultores.backendapicultores.services.ticket;
 
+import com.apicultores.backendapicultores.common.mappers.TicketMapper;
+import com.apicultores.backendapicultores.domain.dto.response.ticket.TicketResponse;
 import com.apicultores.backendapicultores.domain.entities.Ticket;
+import com.apicultores.backendapicultores.exceptions.TicketNotFoundException;
 import com.apicultores.backendapicultores.repositories.TicketRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class getTicketService {
     private final TicketRepository ticketRepository;
+    private final TicketMapper ticketMapper;
 
-    public Ticket getTicketById(UUID ticketId){
-        return;
+    public TicketResponse getTicketById(UUID ticketId){
+        return ticketMapper.toDto(ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket no ha sido encontrado"))
+        );
     }
+
+    public List<TicketResponse> getTicketsByOwner(UUID ownerId){
+        List<Ticket> tickets = ticketRepository.findAll();
+        if (tickets.isEmpty()){
+            throw new TicketNotFoundException("No se ha encontrado ningún ticket");
+        }
+
+        return tickets.stream()
+                .map(ticketMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
 }
