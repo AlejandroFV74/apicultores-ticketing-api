1b3fd0c9ac07dbedc76c22f7ed3a0913da2b6716
Gaenco <00037021@uca.edu.sv>
2026-06-22 23:09:52 -0600

    refactor: enableScheduling

--- backend-apicultores/src/main/java/com/apicultores/backendapicultores/BackendApicultoresApplication.java
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/BackendApicultoresApplication.java
@@ -2,8 +2,10 @@ package com.apicultores.backendapicultores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApicultoresApplication {
 
     public static void main(String[] args) {
