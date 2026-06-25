b8fc8fb883562a8c7e83d8ae48a5a2562f5d9ba1
AlejandroFV74 <00067323@uca.edu.sv>
2026-06-24 18:02:43 -0600

    feat: switch option to enable reservation expired status

--- backend-apicultores/src/main/java/com/apicultores/backendapicultores/scheduler/ReservationExpiryScheduler.java
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/scheduler/ReservationExpiryScheduler.java
@@ -3,18 +3,24 @@ package com.apicultores.backendapicultores.scheduler;
 import com.apicultores.backendapicultores.service.serviceImpl.ReservationExpiryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@ConditionalOnProperty(
+        name = "app.reservation.scheduler.enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
 public class ReservationExpiryScheduler {
 
     private final ReservationExpiryService expiryService;
 
     // Corriendo siempre para prueba
-    @Scheduled(fixedDelayString = "${app.reservation.expiry-check-ms:900000}")
+    @Scheduled(fixedDelayString = "${app.reservation.expiry-check-ms:5000}")
     public void runExpiry() {
         try {
             var expired = expiryService.expireReservations();
