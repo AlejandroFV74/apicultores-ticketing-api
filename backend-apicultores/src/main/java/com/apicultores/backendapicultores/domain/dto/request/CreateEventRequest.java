2e21c001c8a1257b2cdaf0597a9912377a19fd96
AlejandroFV74 <00067323@uca.edu.sv>
2026-06-26 19:31:33 -0600

    feat: automatic configuration for saving seats

--- backend-apicultores/src/main/java/com/apicultores/backendapicultores/domain/dto/request/CreateEventRequest.java
+++ backend-apicultores/src/main/java/com/apicultores/backendapicultores/domain/dto/request/CreateEventRequest.java
@@ -3,11 +3,9 @@ package com.apicultores.backendapicultores.domain.dto.request;
 import com.apicultores.backendapicultores.common.enums.EventStatus;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class CreateEventRequest {
@@ -30,6 +28,6 @@ public class CreateEventRequest {
     private Integer maxTicketsPerUser;
 
     @Valid
-    @NotEmpty(message = "El evento debe contar con asientos")
-    private List<SeatConfigurationRequest> seats;
+    @NotNull(message = "La configuración de asientos es requerida")
+    private SeatingConfigRequest seatingConfig;
 }
